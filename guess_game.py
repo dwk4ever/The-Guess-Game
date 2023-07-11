1708aa6682fb67f48a87cbd8266b3178b4db08c4
dwk4ever <dwk4ever@gmail.com>
2023-07-11 13:37:38 +0000

    Made changes to the conditions

--- guess_game.py
+++ guess_game.py
@@ -7,12 +7,17 @@ guess_number = random.randint(1, 20)
 user_input = 0
 while user_input < 5:
 
+    print("You have 5 chances to guess")
+
 #collecting user input
     user_input = int(input("Enter your guessed number: "))
     if user_input == guess_number:
         print("Congrats! You guessed the correct number!")
-    elif user_input != guess_number:
-        print("Wrong answer, better luck next time!")
+    elif user_input < guess_number:
+        print("Your guess is too low!")
+    elif user_input > guess_number:
+        print("Your guess is too high!")
     else:
-        if user_input > guess_number:
-            print("Your guess is too high!") #clue to guide user
+        if user_input != guess_number:
+            print("Wrong answer, better luck next time!")
+user_input += 1
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # elif user_input < guess_number:
+    #     print("Your guess is too low!")
+    # elif user_input > guess_number:
+    #     print("Your guess is too high!")
