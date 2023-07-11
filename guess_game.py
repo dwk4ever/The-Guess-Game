6b6b550b3665a0213fb9f57a6f762fb9d62e8331
dwk4ever <dwk4ever@gmail.com>
2023-07-11 13:00:03 +0000

    created variable guess_number with random.randint created variable user_input to collect user number created a while loop to give user 5 chances added some conditions to serve as a clue for the user

--- guess_game.py
+++ guess_game.py
@@ -1,4 +1,18 @@
 #Importing random module
 import random
+#correct guessed number
+guess_number = random.randint(1, 20)
 
+#user has 5 chances to guess thhe correct number
+user_input = 0
+while user_input < 5:
 
+#collecting user input
+    user_input = int(input("Enter your guessed number: "))
+    if user_input == guess_number:
+        print("Congrats! You guessed the correct number!")
+    elif user_input != guess_number:
+        print("Wrong answer, better luck next time!")
+    else:
+        if user_input > guess_number:
+            print("Your guess is too high!") #clue to guide user
